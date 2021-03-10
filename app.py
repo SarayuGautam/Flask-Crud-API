45fddd87d55771d290e51bcab44b2003f95bcc0f
Sarayu Gautam <sarayugautam1@gmail.com>
2021-03-10 06:40:47 +0545

    initial app.py setup done

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLALchemy
+from flask_marshmallow import Marshmallow
+import os
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
-def home():
-  return "Hey there!"
 
 if __name__=="__main__":
   app.run(debug=True)
